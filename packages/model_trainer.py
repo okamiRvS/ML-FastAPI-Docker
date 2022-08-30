c27f7318e1f66db830393fbe2a0f3aa765661821
OkamiRvS <okamirvs@gmail.com>
2022-08-30 10:35:44 +0200

    Fix model trainer py

--- packages/model_trainer.py
+++ packages/model_trainer.py
@@ -1,11 +1,10 @@
 from sklearn.metrics import classification_report
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import GaussianNB
 
 def run_model_training(X_train, X_test, y_train, y_test):
-    clf = MultinomialNB()
-    clf.fit(X_train,y_train)
-    clf.score(X_test,y_test)
-    y_pred = clf.predict(X_test)
+    gnb = GaussianNB()
+    gnb.fit(X_train, y_train)
+    y_pred = gnb.predict(X_test)
     print(classification_report(y_test, y_pred))
 
-    return clf
+    return gnb
